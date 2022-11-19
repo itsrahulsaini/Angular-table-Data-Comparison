c65f168d536bf953a8b0b9655a917381a996450c
itsrahulsaini <104614992+itsrahulsaini@users.noreply.github.com>
2022-11-19 21:11:56 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Angular-table-Data-Comparison
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-minimal-material-table-action-buttons-wwpi3s)
---
+++ src/app/app.component.ts
@@ -0,0 +1,349 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
+import { EStore } from '@fireflysemantics/slice';
+import { Observable } from 'rxjs';
+import { untilDestroyed } from 'ngx-take-until-destroy';
+import { DatePipe } from '@angular/common';
+import { invalid } from '@angular/compiler/src/render3/view/util';
+import { FormControl } from '@angular/forms';
+
+/**
+ * The Slice Keys
+ */
+const enum TodoSliceEnum {
+  COMPLETE = 'Complete',
+  INCOMPLETE = 'Incomplete',
+}
+
+class Todo {
+  id: string;
+  description: string;
+  completed: boolean;
+}
+
+const todos: Todo[] = [
+  { id: '123', description: 'Complete me!', completed: false },
+];
+const todos2: Todo[] = [{ id: '321', description: 'Done!', completed: true }];
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [DatePipe],
+})
+export class AppComponent implements OnInit, OnDestroy {
+  displayedColumns: string[] = [
+    'fromDate',
+    'tillDate',
+    'country',
+    'city',
+    'xyz',
+  ];
+  dataSource: MatTableDataSource<any>;
+  dataSource1: MatTableDataSource<any>;
+  xyz: FormControl;
+  constructor(private datePipe: DatePipe) {
+    this.xyz = new FormControl(123);
+  }
+  addressHistory: any = [
+    {
+      fromDate: new Date('2000-01-01'),
+      tillDate: new Date('2000-12-31'),
+      address: 'sdfsdf',
+      city: 'Ambala',
+      state: 'Haryana',
+      country: 'India',
+      pinCode: 'sdfsd',
+      statusInCountry: 'sdfsdf',
+      x: 1,
+    },
+    {
+      fromDate: new Date('2001-01-01'),
+      tillDate: new Date('2001-12-31'),
+      address: 'sdfdsf',
+      city: 'Barwala',
+      state: 'Haryana',
+      country: 'Canada',
+      pinCode: 'sfsd',
+      statusInCountry: 'sdfsd',
+      x: 2,
+    },
+    {
+      fromDate: new Date('2002-01-01'),
+      tillDate: new Date('2002-12-31'),
+      address: 'sdfsd',
+      city: 'Narayangarh',
+      state: 'Haryana',
+      country: 'India',
+      pinCode: 'sdfsd',
+      statusInCountry: 'sdfsd',
+      x: 3,
+    },
+    {
+      fromDate: new Date('2003-01-01'),
+      tillDate: new Date('2003-12-31'),
+      address: 'sdfsd',
+      city: 'Narayangarh',
+      state: 'Haryana',
+      country: 'Canada',
+      pinCode: 'sdfsd',
+      statusInCountry: 'sdfsd',
+      x: 4,
+    },
+    {
+      fromDate: new Date('2004-01-01'),
+      tillDate: new Date('2004-12-31'),
+      address: 'sdfsd',
+      city: 'Narayangarh',
+      state: 'Haryana',
+      country: 'India',
+      pinCode: 'sdfsd',
+      statusInCountry: 'sdfsd',
+      x: 5,
+    },
+    {
+      fromDate: new Date('2005-01-01'),
+      tillDate: new Date('2005-12-31'),
+      address: 'sdfsd',
+      city: 'Narayangarh',
+      state: 'Haryana',
+      country: 'India',
+      pinCode: 'sdfsd',
+      statusInCountry: 'sdfsd',
+      x: 6,
+    },
+  ];
+  personalHistory: any = [
+    {
+      fromDate: new Date('2000-06-01'),
+      tillDate: new Date('2001-06-02'),
+      address: 'sdfsdf',
+      city: 'xyz',
+      state: 'Haryana',
+      country: 'India',
+      pinCode: 'sdfsd',
+      statusInCountry: 'sdfsdf',
+    },
+    // {
+    //   fromDate: '2004-06-01',
+    //   tillDate: '2004-06-02',
+    //   address: 'sdfsdf',
+    //   city: 'xyz',
+    //   state: 'Haryana',
+    //   country: 'Canada',
+    //   pinCode: 'sdfsd',
+    //   statusInCountry: 'sdfsdf',
+    // },
+  ];
+  sortedAddressData: any;
+  validDataFlag: boolean = false;
+  filterAddressGap = [];
+  ngOnInit() {
+    this.dataSource = new MatTableDataSource(this.addressHistory);
+    this.dataSource1 = new MatTableDataSource(this.personalHistory);
+
+    this.dataSource.data = this.sortDataPersonalHistory;
+    this.dataSource1.data = this.personalHistory;
+    this.CompareDta();
+  }
+  ngOnDestroy() {}
+
+  get sortDataPersonalHistory() {
+    return this.addressHistory.sort((a, b) => {
+      return <any>new Date(a.fromDate) - <any>new Date(b.tillDate);
+    });
+  }
+  personlHistorySortByASC(value) {
+    this.sortedAddressData = this.sortDataPersonalHistory.filter(
+      (x) => x.country == value.country
+    );
+    return this.sortedAddressData;
+  }
+
+  // validateAddressData() {
+  //   debugger;
+  //   let errorData = [];
+
+  //   for (let i = 0; i < this.personalHistory.length; i++) {
+  //     this.personlHistorySortByASC(this.personalHistory[i]);
+  //     // for (let j = 0; j < this.sortedAddressData.length; j++) {
+  //     // if (
+  //     //   new Date(this.personalHistory[i].fromDate) <=
+  //     //     new Date(this.addressHistory[j].fromDate) &&
+  //     //   new Date(this.personalHistory[i].tillDate) >=
+  //     //     new Date(this.addressHistory[j].tillDate) &&
+  //     //   this.personalHistory[i].country.toLowerCase() !=
+  //     //     this.addressHistory[j].country.toLowerCase()
+  //     // ) {
+  //     //   errorData.push(this.addressHistory[j]);
+  //     //   console.log(errorData);
+  //     //   alert('Done');
+  //     // }
+  //     let data = this.sortedAddressData.filter(
+  //       (x) =>
+  //         new Date(this.personalHistory[i].fromDate) >= new Date(x.fromDate) &&
+  //         new Date(this.personalHistory[i].tillDate) <= new Date(x.tillDate)
+  //     );
+  //     console.log(data);
+
+  //     //  else if (
+  //     //   this.personalHistory[i].fromDate >= this.addressHistory[j].fromDate &&
+  //     //   this.personalHistory[i].tillDate <= this.addressHistory[j].tillDate &&
+  //     //   this.personalHistory[i].country == this.addressHistory[j].country
+  //     // ) {
+  //     //   alert('Done');
+  //     // }
+  //     // else {
+  //     //   alert('Done');
+  //     //   // break
+  //     // }
+  //     // }
+  //   }
+  // }
+  validateAddressData() {
+    debugger;
+    for (let i = 0; i < this.personalHistory.length; i++) {
+      this.validDataFlag = true;
+      let spaceFlag = false;
+      let noSpaceFlag = false;
+      let spaceStartIndex = [];
+      this.filterAddressGap = [];
+      spaceStartIndex.push(0);
+      this.personlHistorySortByASC(this.personalHistory[i]);
+      if (this.sortedAddressData.length < 1) {
+        this.validDataFlag = false;
+        break;
+      }
+      for (let j = 0; j < this.sortedAddressData.length; j++) {
+        let toDate = new Date(this.sortedAddressData[j]?.tillDate);
+        let nextFromDate: any = new Date(
+          this.sortedAddressData[j + 1]?.fromDate
+        );
+        let daysDifference =
+          Math.ceil(nextFromDate.getTime() - toDate.getTime()) /
+          (1000 * 3600 * 24);
+        let filterGapData = {
+          index: j,
+          isGap: false,
+        };
+        if (nextFromDate == 'Invalid Date') {
+          this.filterAddressGap.push(filterGapData);
+          if (this.sortedAddressData.length <= 1) {
+            noSpaceFlag = true;
+          }
+        }
+
+        if (daysDifference > 1) {
+          filterGapData.isGap = true;
+
+          spaceFlag = true;
+          this.filterAddressGap.push(filterGapData);
+        } else if (daysDifference <= 1) {
+          noSpaceFlag = true;
+          this.filterAddressGap.push(filterGapData);
+        }
+      }
+      if (noSpaceFlag == true && spaceFlag == false) {
+        let length = this.sortedAddressData.filter(
+          (x) => x.country == this.personalHistory[i].country
+        ).length;
+        let addressStartDate = this.sortedAddressData[0].fromDate;
+        let addressTillDate = this.sortedAddressData[length - 1]?.tillDate;
+        if (
+          addressStartDate <= this.personalHistory[i].fromDate &&
+          addressTillDate >= this.personalHistory[i].tillDate
+        ) {
+          this.validDataFlag = true;
+          break;
+        } else {
+          this.validDataFlag = false;
+          break;
+        }
+      } else if (spaceFlag == true) {
+        for (let k = 0; k < this.filterAddressGap.length; k++) {
+          let addressStartDate =
+            this.sortedAddressData[spaceStartIndex[0]].fromDate;
+          let addressTillDate =
+            this.sortedAddressData[this.filterAddressGap[k].index].tillDate;
+          if (
+            new Date(addressStartDate) <=
+              new Date(this.personalHistory[i].fromDate) &&
+            new Date(addressTillDate) >=
+              new Date(this.personalHistory[i].tillDate)
+          ) {
+            this.validDataFlag = true;
+            break;
+          } else {
+            this.validDataFlag = false;
+          }
+          if (this.filterAddressGap[k].isGap == true) {
+            spaceStartIndex = [];
+            spaceStartIndex.push(this.filterAddressGap[k + 1]?.index);
+          }
+        }
+      }
+    }
+    console.log(this.validDataFlag);
+  }
+  indexof() {
+    console.log(this.addressHistory.indexOf(0));
+  }
+
+  CompareDta() {
+    let data1 = {
+      rowId: null,
+      isRelativeInCanada: true,
+      relativeRelationInCanada: '                     Adopted Sister',
+      relativeNameInCanada: '     ssa',
+      relativeAddressInCanada: 'df',
+      relativeStatusInCanada: 'Permanent Resident',
+      isSpouseRelativeInCanada: null,
+      spouseRelativeRelationInCanada: null,
+      spouseRelativeNameInCanada: null,
+      spouseRelativeAddressInCanada: null,
+      spouseRelativeStatusInCanada: null,
+    };
+
+    let data2 = {
+      rowId: null,
+      isRelativeInCanada: true,
+      relativeRelationInCanada: 'ghjg',
+      relativeNameInCanada: 'ssa',
+      relativeAddressInCanada: 'df',
+      relativeStatusInCanada: 'Permanent Resident',
+      isSpouseRelativeInCanada: null,
+      spouseRelativeRelationInCanada: null,
+      spouseRelativeNameInCanada: null,
+      spouseRelativeAddressInCanada: null,
+      spouseRelativeStatusInCanada: null,
+    };
+    debugger;
+
+    let c = 'dfgdfg        ';
+    alert(c.trim());
+    let x = JSON.parse(JSON.stringify(data1).replace(/"\s+|\s+"/g, '"'));
+    let y = JSON.parse(JSON.stringify(data2).replace(/"\s+|\s+"/g, '"'));
+    let result = this.CompareData(x, y);
+    // alert(result);
+  }
+
+  CompareData(x, y) {
+    if (x === y) return true;
+    if (!(x instanceof Object) || !(y instanceof Object)) return false;
+    if (x.constructor !== y.constructor) return false;
+    for (var p in x) {
+      if (!x.hasOwnProperty(p)) continue;
+      if (!y.hasOwnProperty(p)) return false;
+      if (x[p] === y[p]) continue;
+
+      if (typeof x[p] !== 'object') return false;
+
+      if (!this.CompareData(x[p], y[p])) return false;
+    }
+
+    for (p in y) if (y.hasOwnProperty(p) && !x.hasOwnProperty(p)) return false;
+
+    return true;
+  }
+}
